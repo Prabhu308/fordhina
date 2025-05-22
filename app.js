10a8dd93dd90a661f811138f62ed19e4c92104df
Prabhu <prabu502@gmail.com>
2025-05-22 16:29:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('<H1 style="color:red">Hello, World!<H1>');
 });
 
 const PORT = process.env.PORT || 3000;
